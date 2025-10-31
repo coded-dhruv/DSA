c98b244657dd6f20a6fdb6f25f0f410cdb974fbb
Dhruv <dhruvxjain@gmail.com>
2025-10-31 13:59:47 +0530

    Add files via upload

---
+++ dsa.js
@@ -0,0 +1,131 @@
+// SUMMING
+
+// let a = "10";
+// let b = "20";
+
+// let c = a + b;
+
+// console.log(c);
+
+// string + string = string (concetanation)
+// string + number = string (concetanation)
+// num + num = num (Arithamatic)
+
+// let a = 10;
+// let b = 20;
+
+// console.log ("The sum of " + a + " and " + b + " is " + a + b);
+// //The sum of 10 + and + b + is + a + b
+// //The sum of 10 and + b + is + a + b
+// //The sum of 10 and 20 + is + a + b
+// //The sum of 10 and 20 is + a + b
+// //The sum of 10 and 20 is 10 + b
+// //The sum of 10 and 20 is 1020
+
+// console.log ("The sum of " + a + " and " + b + " is " + (a + b));
+
+// let c = Number(prompt("Enter your number"));
+
+// let d = Math.floor(c);
+
+// console.log(d);
+
+// let num1 = 10;
+// let num2 = 20;
+
+// let c = num1;
+
+// num1 = num2;
+
+// num2 = c;
+
+// [a,b] = [10, 20];
+
+// [b, a] = [a, b];
+
+// console.log(a,b)
+
+// let a = 1;
+// let b = a++;
+
+// console.log(a);
+// console.log(b);
+
+// let i = 11; 
+
+// i = i++ + ++i; 
+
+// console.log(i);
+
+
+// let a = 11, b = 22, c; 
+
+// c = a + b + a++ + b++ + ++a + ++b; 
+
+// console.log("a=" + a); 
+
+// console.log("b=" + b); 
+
+// console.log("c=" + c);et i = 0; 
+
+// let i = 0;
+
+// i = i++ - --i + ++i - i--; 
+
+// console.log(i);
+
+// let b = false; 
+
+// b++; 
+
+// console.log(b);
+
+// let i = 1, j = 2, k = 3; 
+
+// let m = i-- - j-- - k--; 
+
+// console.log("i=" + i); 
+
+// console.log("j=" + j); 
+
+// console.log("k=" + k); 
+
+// console.log("m=" + m);
+
+// let a = 1, b = 2; 
+
+// console.log(--b - ++a + ++b - --a);
+
+// let i = 19, j = 29, k; 
+
+// k = i-- - i++ + --j - ++j + --i - j-- + ++i - j++; 
+
+// console.log("i=" + i); 
+
+// console.log("j=" + j); 
+
+// console.log("k=" + k);
+
+// let i = 11; 
+
+// let j = --(i++);
+
+// let m = 0, n = 0; 
+
+// let p = --m * --n * n-- * m--; 
+
+// console.log(p);
+
+// let a = 1; 
+
+// a = a++ + ++a * --a - a--; 
+
+// console.log(a);
+
+// let a = 11++; 
+
+// console.log(a);
+let i = 0, j = 0; 
+
+console.log(--i * i++ * ++j * j++);
+
