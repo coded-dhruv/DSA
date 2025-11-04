d8afb11f4df58adb513dc68937cf3547c5498e18
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-04 11:41:03 +0530

    Herons Formula

--- DAY02/day2.js
+++ DAY02/day2.js
@@ -12,12 +12,31 @@
 
 // C.I =(P*(1  + R/100)^T) - P
 
-var p = Number(prompt("Enter Principle"));
-var r = Number(prompt("Enter Rate"));
-var t = Number(prompt("Enter Time"));
+// var p = Number(prompt("Enter Principle"));
+// var r = Number(prompt("Enter Rate"));
+// var t = Number(prompt("Enter Time"));
 
-var a = p* Math.pow(1 + r/100, t);
+// var a = p* Math.pow(1 + r/100, t);
 
-var CI = a - p;
-console.log(CI);
+// var CI = a - p;
+// console.log(CI);
+
+// HERON's Formula
+
+//s = (a + b + c)/2
+
+//sqrt(s*(s-a)*(s-b)*(s-c))
+
+var a = Number(prompt("Enter side a"));
+var b = Number(prompt("Enter side b"));
+var c = Number(prompt("Enter side c"));
+
+if(a + b > c || a + c > b || b + c > a){
+    var s = (a + b + c)/2;
+    console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+} else {
+    console.log("This is not a Triangle");
+}
+
+
 
