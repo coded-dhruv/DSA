084baa62a6fba9dc2bab79f897d3bb482edb4ffc
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-04 12:14:05 +0530

    Shop Discount

--- DAY03/day3.js
+++ DAY03/day3.js
@@ -12,3 +12,32 @@
 // } if(a == b){
 //     console.log("Both are equal");
 // }
+
+// Shop Discount
+// 0 - 100 = 0%
+// 100 - 500 = 20%
+// 500 - 1000 = 40%
+// more than 1000 = 50%
+
+var total = Number(prompt("Enter total bill"));
+if(total <= 100 && total >= 0){
+    var finalPrice = total;
+    var dc = "-0%";
+} if(total>100 && total <= 500){
+    var finalPrice = total * 0.8;
+    var dc = "-20%"
+} if(total > 500 && total <= 1000){
+    var finalPrice = total * 0.6;
+    var dc = "-40%"
+} if(total > 1000){
+    var finalPrice = total*0.5;
+    var dc = "-50%"
+}
+
+var Price = document.getElementById('Price');
+var final = document.getElementById('final');
+var discount = document.getElementById('discount');
+
+Price.innerHTML = total;
+final.innerHTML = finalPrice;
+discount.innerHTML = dc;
