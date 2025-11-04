e4f4868458bc0a65b8fd963371e7933989a3183e
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-04 11:18:44 +0530

    DAY 2

---
+++ DAY02/day2.js
@@ -0,0 +1,20 @@
+// console.log(Math.floor(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.round(4.2));
+// console.log(Math.trunc(2.9823798));
+// console.log(Math.sqrt(9));
+// console.log(Math.cbrt(27));
+// console.log(Math.abs(-2));
+// console.log(Math.max(2, 3));
+// console.log(Math.min(2, 3, 1));
+// console.log(Math.floor(Math.random()*900 + 100));
+// console.log(Math.pow(2, 3))
+
+// C.I =(P*(1  + R/100)^T) - P
+
+var p = Number(prompt("Enter Principle"));
+var r = Number(prompt("Enter Rate"));
+var t = Number(prompt("Enter Time"));
+
+
+
