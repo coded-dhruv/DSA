154f300b3ed8ca4e7fad59d928c2bf8730839307
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-04 11:28:51 +0530

    CI

--- DAY02/day2.js
+++ DAY02/day2.js
@@ -16,5 +16,8 @@ var p = Number(prompt("Enter Principle"));
 var r = Number(prompt("Enter Rate"));
 var t = Number(prompt("Enter Time"));
 
+var a = p* Math.pow(1 + r/100, t);
 
+var CI = a - p;
+console.log(CI);
 
