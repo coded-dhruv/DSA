8d7f99755ed8ade3955ca817d637c3969c4b070a
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-05 13:59:38 +0530

    Print hello world n times

---
+++ DAY04/day4.js
@@ -0,0 +1,16 @@
+//LOOPS
+
+// For                  // Entry Point
+// While                // Exit Point
+// Do while
+// Nested
+// ForEach
+
+//Q 17. Accept an integer and Print hello world n times.
+
+var number = Number(prompt("ENter the number"));
+//
+//
+for (i=1;i<=number;i++){
+    console.log("Hello world");
+}
