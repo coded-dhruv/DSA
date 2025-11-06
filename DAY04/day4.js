bc1e573faf7a9aba7ef381a13132ac8df67d6b3d
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-06 14:09:50 +0530

    Swap

--- DAY04/day4.js
+++ DAY04/day4.js
@@ -8,9 +8,19 @@
 
 //Q 17. Accept an integer and Print hello world n times.
 
-var number = Number(prompt("ENter the number"));
-//
-//
-for (i=1;i<=number;i++){
-    console.log("Hello world");
+// var number = Number(prompt("ENter the number"));
+// //
+// //
+// for (i=1;i<=number;i++){
+//     console.log("Hello world");
+// }
+
+////////
+
+var num = Number(prompt("Enter the number"));
+
+var isPrime = true;
+for(i = 2; num%i == 0; i++){
+    var isPrime = false;
+    break;
 }
---
+++ DAY05/day5.js
@@ -0,0 +1,25 @@
+// Break & Continue
+
+// for(i = 0; i<=10; i++){
+//     if(i==5) continue
+//     console.log(i);
+// }
+
+// While Loop
+
+// i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+var num = Number(prompt("Enter"));
+
+var a = num % 10;
+var b = Math.floor(num/10);
+var c = a * 10
+
+console.log (c + b);
+
+
