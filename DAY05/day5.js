1aeb42039665866c422aa3a9f654dffbc6988fbb
Dhruv Jain <dhruvxjain@gmail.com>
2025-11-06 14:39:43 +0530

    Switch Case

--- DAY05/day5.js
+++ DAY05/day5.js
@@ -14,12 +14,35 @@
 // }
 
 
-var num = Number(prompt("Enter"));
 
-var a = num % 10;
-var b = Math.floor(num/10);
-var c = a * 10
 
-console.log (c + b);
+// var a = num % 10;
+// var b = Math.floor(num/10);
+// var c = a * 10
 
+// console.log (c + b);
+
+// var num = Number(prompt("Enter"));
+
+// var sqr = Math.pow(num, 2);
+
+// if(sqr % 10 == num){
+//     console.log(`${num} is a Automorphic Number`);
+// } else{
+//     console.log(`${num} is not a Automorphic Number`)
+// }
+
+// Switch Case
+
+// var a = Number(prompt("Enter between 1 - 4"));
+
+// switch(a){
+//     case 1: console.log("Monday");
+//     case 2: console.log("Tuesday");
+//     case 3: console.log("Wednesday");
+//     break
+//     case 4: console.log("Thursday");
+//     break
+//     default: console.log("Mai to chalunga");
+// }
 
